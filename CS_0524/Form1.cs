0f0186d4c7e413e4aa1360f6608182a359f99b1b
Arakawa Ryu <104044024+ryu-ua@users.noreply.github.com>
2024-05-24 15:02:52 +0900

    コントロール

--- CS_0524/Form1.cs
+++ CS_0524/Form1.cs
@@ -6,5 +6,23 @@ namespace CS_0524
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+            label1.Text = "Hello";
+            MessageBox.Show($"‚±‚ñ‚É‚¿‚Í{textBox1.Text}‚³‚ñ");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 10;
+            label1.Top = label1.Top + 10;
+        }
     }
 }
